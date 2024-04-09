d953998fca8bf7dcf4233cad8ca2f23fe9f499d4
FatmaElMahdi1000 <f69246984@gmail.com>
2024-04-09 16:11:50 -0700

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,31 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
  *bubble_sort - Bubble sort algorithm
  *@array: the array
  *@size: size or length of the array
  *
- * Return: sorted array in ascending order
 **/
-
-
 
 void bubble_sort(int *array, size_t size)
 {
+	for (int i = 0; i < size ; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
 
+	{
+		if (array[j] > array[j + 1])
+	{
+		int temp = array[j];
+
+		array[j] = array[j + 1];
+		array[j + 1] = temp;
+	}
+	}
+	}
+	for (int i = 0; i < size; i++)
+	{
+		print_array(array, size);
+	}
+
+	return (0);
 }
--- sort.h
+++ sort.h
@@ -2,6 +2,9 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
