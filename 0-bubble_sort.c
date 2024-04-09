0d8e2a41c5fb82212d114b3c34906240d65fbe1b
FatmaElMahdi1000 <f69246984@gmail.com>
2024-04-09 14:56:19 -0700

    sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ *bubble_sort - Bubble sort algorithm
+ *@array: the array
+ *@size: size or length of the array
+ *
+ * Return: sorted array in ascending order
+**/
+
+
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
